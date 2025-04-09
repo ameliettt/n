61561c22baccc7820d7b0b35a35857fca023f0be
ameliettt <amelie811005@gmail.com>
2025-04-09 21:47:47 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const skewSetter = gsap.quickTo(".images img", "skewY");
+  const clamp = gsap.utils.clamp(-20, 20);
+
+  ScrollSmoother.create({
+    wrapper: "#smooth-wrapper",
+    content: "#smooth-content",
+    smooth: 2,
+    speed: 1.5,
+    effects: true,
+    onUpdate: (self) => skewSetter(clamp(self.getVelocity() / -50)),
+    onStop: () => skewSetter(0)
+  });
+});
