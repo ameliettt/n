dfa685498d3b423f2e5c68d3eff0a2857bf2ade1
ameliettt <amelie811005@gmail.com>
2025-04-09 21:55:25 +0800

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   // 可選加上動態 skew 效果
-  const skewSetter = gsap.quickTo(".gallery img", "skewY"),
+  const skewSetter = gsap.quickTo(".images img", "skewY"),
         clamp = gsap.utils.clamp(-20, 20);
 
   smoother.scrollTrigger?.addEventListener("update", (self) => {
